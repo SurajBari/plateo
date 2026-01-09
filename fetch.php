2ccb927de4812794121a9ad7896a92960179e5c0
SURAJ BARI <124048373+SurajBari@users.noreply.github.com>
2026-01-09 13:20:38 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,6 +1,5 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+session_start();
 
 $products = [
     'bt-01' => [
@@ -12,17 +11,17 @@ $products = [
     ],
     'kg-02' => [
         'name' => 'Classic Matir Glass',
-        'type' => 'Earthen Tumbler',
+        'type' => 'Earthen Tumbler (Set of 2)',
         'price' => 249,
         'img' => 'https://5.imimg.com/data5/SELLER/Default/2021/6/MQ/NP/YI/131065961/traditional-clay-glass-500x500.jpg',
         'desc' => 'Tapered earthen glass that keeps water naturally cool with a rustic terracotta aroma.'
     ],
-    'cp-03' => [
-        'name' => 'Artisan Clay Cooking Pot',
-        'type' => 'Traditional Clay Pot',
-        'price' => 899,
-        'img' => 'https://m.media-amazon.com/images/I/61SUnmFfR9L._SL1100_.jpg',
-        'desc' => 'Hand-crafted clay pot for authentic slow cooking. Retains nutrients and adds an earthy flavor.'
+    'as-03' => [
+        'name' => 'Alpana Engraved Bowl',
+        'type' => 'Hand-Painted Stoneware',
+        'price' => 399,
+        'img' => 'https://m.media-amazon.com/images/I/61MvS1PAn6L._SL1000_.jpg',
+        'desc' => 'Deep bowl featuring hand-carved traditional folk motifs on a dark burnt clay base.'
     ],
     'bs-04' => [
         'name' => 'Royal Baishakhi Set',
@@ -33,5 +32,28 @@ $products = [
     ]
 ];
 
-echo json_encode($products);
-exit;
+if (!isset($_SESSION['cart'])) $_SESSION['cart'] = [];
+
+// Handle Actions
+$action = $_REQUEST['action'] ?? 'get_all';
+
+if ($action == 'add') {
+    $id = $_POST['product_id'];
+    $_SESSION['cart'][$id] = ($_SESSION['cart'][$id] ?? 0) + 1;
+    echo json_encode(['success' => true, 'last_added' => $products[$id]['name']]);
+    exit;
+}
+
+if ($action == 'remove') {
+    $id = $_GET['product_id'];
+    unset($_SESSION['cart'][$id]);
+    echo json_encode(['success' => true]);
+    exit;
+}
+
+// Default: Return Products and Cart State
+echo json_encode([
+    'products' => $products,
+    'cart' => $_SESSION['cart'],
+    'cart_count' => array_sum($_SESSION['cart'])
+]);
